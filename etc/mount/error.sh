ed4ad2be25b1ba7b87b105b1da03e9c1cda712e8
ljf10000 <l_j_f@163.com>
2015-04-13 15:30:50 -0400

    ok.sh

--- etc/mount/error.sh
+++ etc/mount/error.sh
@@ -3,8 +3,9 @@
 main() {
 	local dev="$1"
 	local dir="$2"
+	local err="$3"
 
-
+	echo "mount ${dev} ${dir} error:${err}"
 }
 
 main "$@"
--- etc/mount/ok.sh
+++ etc/mount/ok.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
 
+rootfs0_ok() {
+	mkdir -p /tmp/appkey
+	cp -fpR /etc/appkey/* /tmp/appkey/
+	echo "copy appkey..."
+}
+
+config_ok() {
+	local file
+	local cfile #cloud config file
+	local dfile #default config rule
+	local rfile #run config file
+	local sfile #source config file
+
+	while read file; do
+		cfile="/mnt/flash/config/${file}"
+		dfile="/${file}"
+		rfile="/tmp/config/${file}"
+		sfile=${dfile}
+
+		mkdir -p $(dirname ${rfile})
+
+		if [[ -f "${cfile}" ]]; then
+			sfile=${cfile}
+		fi
+
+		echo "use ${sfile}"
+		cp -f ${sfile} ${rfile}
+	done < /etc/config.list
+}
+
+data_ok() {
+
+}
+
 main() {
 	local dev="$1"
 	local dir="$2"
 
-
+	case ${dir} in
+	rootfs0)
+		rootfs0_ok
+		;;
+	config)
+		config_ok
+		;;
+	data)
+		data_ok
+		;;
+	*)
+		;;
+	esac
 }
 
 main "$@"
