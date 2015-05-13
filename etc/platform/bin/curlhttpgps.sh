4ccd8ca7fbffde32bf1c94bbf1090595136548e8
zhangsiyu <zhangsiyu@debian6.(none)>
2015-05-13 18:31:15 -0400

    modify for push log 2015-5-13

---
+++ etc/platform/bin/curlhttpgps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+. /etc/platform/bin/platform.in
+. /etc/utils/utils.in
+
+gps_upload() {
+
+	local GPS_LOG=/tmp/gps_out.log
+	local GPS_EN=/etc/platform/conf/gps_en
+	local MAC=$(cat ${FILE_REGISTER} | jq -j ".mac" | tr  ":" "-")
+	local status info
+
+for upload in $(ls /tmp/.log/gps/gps-* | sort -r) 
+do
+	status=$(curl --max-time 180  \
+		-F "type=gps" \
+		-F "signature=$(cat ${GPS_EN})" \
+		-F "ident=${MAC}" \
+		-F "content=$(cat ${upload})"  \
+		-o  ${GPS_LOG} \
+		-s \
+		-w  %{http_code}  \
+		http://update1.9797168.com:821/wifibox/)
+        if [ "$status" -eq "200" ];then
+                outcontent=$(cat ${GPS_LOG} | jq -j ".success")
+		case ${outcontent} in
+		true)
+			info="ok"
+			;;
+		false)
+			info="failed"
+			;;
+		*)
+			info="error"
+			;;
+		esac
+		
+		echo "$(date '+%F-%H:%M:%S') upload ${upload} ${info}" >> ${GPS_LOG}
+
+		rm -f ${upload}
+	else
+		echo "$(date '+%F-%H:%M:%S') upload ${upload} status=${status}" >> ${GPS_LOG}
+	fi
+	
+	sleep 1
+done
+}
+
+main() {
+	exec_with_flock	/tmp/.gps_upload.lock gps_upload
+}
+
+main "$@"
+
