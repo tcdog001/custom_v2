1a093fe90633f0adcf7a791e09bd0ba23bd2ff7a
ljf10000 <l_j_f@163.com>
2015-06-05 10:33:18 +0800

    dir.in

--- etc/upgrade/set_dir.sh
+++ etc/upgrade/set_dir.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-. /etc/utils/dir.in
 
-set_tmp_log_dir() {
-    local dir
-
-    for dir in ${!dir_tmp_log_*}; do
-        mkdir -p ${!dir}
-    done
-}
+. ${__ROOTFS__}/etc/utils/dir.in
 
 main() {
-	set_tmp_log_dir
-	mkdir -p ${dir_opt_log_onoff}
-	mkdir -p ${dir_opt_log_drop_3g}
-	mkdir -p ${dir_tftp}
-	mkdir -p ${dir_run_config}
+	setup_dir_default
 }
+
 main "$@"
