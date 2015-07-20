d82fb94a3ddc6de9c431c6d0796b87099e651bc2
guoweibo <guoweibo@debian6.(none)>
2015-07-20 17:28:38 -0400

    rewrite register.sh

--- etc/platform/bin/register.sh
+++ etc/platform/bin/register.sh
@@ -2,64 +2,10 @@
 
 . /etc/platform/bin/platform.in
 
-CERTIFICATE=/etc/platform/conf/server.cer
-USER_PASSWD=autelanauteviewlms:autelanauteviewlms20140925
-URL_PATH=/etc/platform/conf/platform.json
-URL_DEFAULT=https://atbus.9797168.com:8443/LMS/lte/
-COOKIE_FILE=/tmp/registerCookie.txt
-PAGE=lteRegister.do
-RESULT_FILE=/tmp/registerResult.txt
-RESPONSE_FILE=/tmp/registerResponse.txt
-CURLE_OK=0
-CURLE_COULDNT_CONNECT=7
-CURLE_COULDNT_RESOLVE_HOST=6
-RECONNECTION_INTERVAL=5
-if [ -f $RESULT_FILE ];then
-	rm $RESULT_FILE
-fi
+main() {
+	local URL_PATH=/etc/platform/conf/platform.json
+	local URL_DEFAULT=https://lms1.9797168.com:8443/LMS/lte/
 
-while [ ! -f $FILE_REGISTER ]; do
-	sleep 5
-done
-
-if [ -f $FILE_REGISTER ];then
-	if [ -s $URL_PATH ];then
-		url_path=`cat $URL_PATH |jq -j '.url'`
-		url=${url_path}${PAGE}
-	else
-		url=${URL_DEFAULT}${PAGE}
-	fi
-	
-	while true
-	do
-		curl  -k  -cert $CERTIFICATE  -u $USER_PASSWD  -H "Content-type: application/json"  -X POST  -d @$FILE_REGISTER -s  -c $COOKIE_FILE  $url > $RESULT_FILE
-		response=$?
-		echo response=$response > ${RESPONSE_FILE}
-#		if [ $response -eq $CURLE_COULDNT_CONNECT ] || [ $response -eq $CURLE_COULDNT_RESOLVE_HOST ];then
-		if [ $response != $CURLE_OK ]; then
-			sleep $RECONNECTION_INTERVAL
-		else
-			break
-		fi
-	done
-
-	out=`cat $RESULT_FILE |jq '.code'`
-	case $out in
-		0) 
-			echo "register success!"
-			;;
-		-1)	
-			echo "user/password error!"
-			;;
-		*) 
-			echo "register fail!"
-			;;
-	esac
-#	if [ -f $RESULT_FILE ];then
-#		rm $RESULT_FILE
-#	fi
-else
-	echo $FILE_REGISTER" is not exist!"
-fi
-
-
+	register_operation  "${URL_PATH}" "${URL_DEFAULT}"
+}
+main "$@"
