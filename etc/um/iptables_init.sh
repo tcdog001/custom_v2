6d22f8be1d4301436761fe549232ccc3c6739b03
liuhuijun <liuhuijun@e830.(none)>
2015-08-27 22:07:33 +0800

    modify iptables_init.sh for white_list ACCEPT

--- etc/um/iptables_init.sh
+++ etc/um/iptables_init.sh
@@ -8,7 +8,7 @@ iptables -P  FORWARD DROP
 ipset -N white_list ipmap --network 192.168.0.0/16
 iptables -A FORWARD  -p udp --dport 53 -m state --state NEW -j ACCEPT
 
-iptables -A FORWARD  -i eth0.1 -o ppp0 -m set ! --match-set  white_list src -j DROP
+iptables -A FORWARD  -i eth0.1 -o ppp0 -m set --match-set  white_list src -j ACCEPT
 iptables -A FORWARD  -p icmp --icmp-type 8 -j ACCEPT
 iptables -A FORWARD  -p tcp --dport 443 --syn -m state --state NEW -j ACCEPT
 iptables -A FORWARD  -p tcp --dport 21 --syn -m state --state NEW -j ACCEPT
