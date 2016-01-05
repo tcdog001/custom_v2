dcf2dae189cc21e42a2f88268ff77e7bd196874e
liuhuijun <liuhj@autelan.com>
2016-01-05 15:34:33 +0800

    .

--- etc/init.d/php-fpm_start.sh
+++ etc/init.d/php-fpm_start.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -f /tmp/config/php/php-fpm.ini ]]; then
 	mkdir -p /tmp/config/
-	cp -r /usr/local/php/ /tmp/config/php
+	cp -r /usr/local/php/ /tmp/config
 fi
 
 #php-fpm -y /tmp/config/php/php-fpm.ini 2> /dev/null
